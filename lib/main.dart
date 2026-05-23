9cf68736fa0da495965026f673acda7e0c2f1fff
FANGNON Mawouégnigan Grégoire <fmgexcellence@gmail.com>
2026-05-23 10:27:19 +0100

    Standardisation main.dart with SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_task_app/utils/constants.dart';
-// import 'package:flutter_task_app/views/screens/onboarding_screen.dart';
-// import 'package:flutter_task_app/views/screens/splash_screen.dart';
+import 'package:flutter_task_app/views/screens/splash_screen.dart';
 import 'package:flutter_task_app/views/screens/login_screen.dart';
-// import 'package:flutter_task_app/views/screens/register_screen.dart';
-// import 'package:flutter_task_app/views/screens/home_screen.dart';
-//import 'package:flutter_task_app/views/screens/add_editing_task_screen.dart';
-
+import 'package:flutter_task_app/views/screens/register_screen.dart';
+import 'package:flutter_task_app/views/screens/home_screen.dart';
 
 void main() {
-  // Configuration de la bare d'etat
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
-      statusBarColor: AppColors.background, 
+      statusBarColor: AppColors.background,
       statusBarIconBrightness: Brightness.dark,
       statusBarBrightness: Brightness.light,
     ),
   );
-  runApp(const MyApp());
+
+  runApp(const TaskApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TaskApp extends StatelessWidget {
+  const TaskApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TaskFlow',
       theme: AppTheme.lightTheme,
-      home: LoginScreen(), //AddEditingTaskScreen(mode: 'Edit'),  // OnboardingScreen() SplashScreen() LoginScreen() RegisterScreen() HomeScreen() AddEditingTaskScreen(mode: [Add, Edit])
+
+      // POINT D’ENTRÉE UNIQUE
+      home: const SplashScreen(),
     );
   }
 }
